0efd00b60d36d5b97167e579ba186e3449be2cc0
Eric LaFlure <ejlaflure@gmail.com>
2020-05-13 21:56:36 -0500

    adding more data and queries

--- Queries/Queries.sql
+++ Queries/Queries.sql
@@ -119,3 +119,69 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+-- Joining tables into new employee_info table
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.gender,
+    s.salary 
+INTO employee_info
+FROM employees as e
+LEFT JOIN salaries as s
+ON e.emp_no = s.emp_no;
+
+-- Joining tables into new emp_info table
+SELECT e.emp_no,
+	e.first_name,
+    e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+     AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+ON (dm.emp_no = ce.emp_no);
+
+-- List of retirees per department
+SELECT de.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name	
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Create new table for retiring employees in sales
+SELECT emp_no, first_name, last_name, dept_name
+INTO sal_dev_info
+FROM dept_info
+WHERE dept_name = 'Sales'
+OR dept_name = 'Development';
+
